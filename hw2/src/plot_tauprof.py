cbbb3f1cb1d7731640d79e66ed59cf2dda6515ab
tanmoy7989 <tanmoy.7989@gmail.com>
2016-10-17 15:07:28 -0700

    final version submitted

--- hw2/src/plot_tauprof.py
+++ hw2/src/plot_tauprof.py
@@ -5,8 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-fdict = {0:'matvec', 1:'ddot', 2:'daxpy', 3:'gen_B', 4:'save_vec', 
-		 5:'MPI_Send', 6:'MPI_Recv', 7:'MPI_Init', 8:'MPI_Barrier', 9:'MPI_Finalize', 10:'MPI_Allreduce'}
+fdict = {0:'matvec', 1:'ddot', 2:'daxpy', 3:'gen_B',
+		 4:'MPI_Send', 5:'MPI_Recv', 6:'MPI_Init', 7:'MPI_Barrier', 8:'MPI_Finalize', 9:'MPI_Allreduce'}
 Nfunc = len(fdict.keys())
 data = np.loadtxt('tauprof.dat')
 
@@ -20,15 +20,15 @@ axlist = []
 for key in range(Nfunc):
 	val = fdict[key]
 	x = pvals ; y = data[:, key+2]
-	ax = fig.add_subplot(3,5,ind)
-	ax.plot(x, y, linewidth = 3, marker = 'o', markersize = 8)
-	ax.set_title(val)
+	ax = fig.add_subplot(2,5,ind)
+	ax.plot(x, y, linewidth = 3, marker = 'o', markersize = 8, label = val)
+	ax.legend(loc = 'best', fontsize = 20, frameon = False)
 
 	axlist.append(ax)
 	ind += 1
 
-plt.figtext(0.45, 0.02, 'number of processors', fontsize = 25)
-plt.figtext(0.02, 0.6, 'total inclusive time (msec)', fontsize = 25, rotation = 90)
+plt.figtext(0.42, 0.02, 'number of processors', fontsize = 25)
+plt.figtext(0.02, 0.65, 'total inclusive time (msec)', fontsize = 25, rotation = 90)
 plt.tight_layout()
 plt.savefig(figname)
 
