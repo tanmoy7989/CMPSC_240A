e8930e98eaca5fcb2df36c504faf4074f57822ac
tanmoy7989 <tanmoy.7989@gmail.com>
2016-10-27 04:10:14 -0700

    rexlib working; need to prune swap move in TSP MC

--- final_project/TSP/plotene.py
+++ final_project/TSP/plotene.py
@@ -8,15 +8,6 @@ coords = np.loadtxt('p01_xy.txt', skiprows = 1)
 dist = np.loadtxt('p01_d.txt', skiprows = 1)
 ncities = len(coords)
 
-
-def calcEne(tour):
-	d = 0.0
-	for ii in range(ncities):
-		i = tour[ii] ; j = tour[ii+1]
-		d += dist[i, j]
-	return d
-
-
 def plotTour(ax, tour):
 	for ii in range(ncities + 1):
 		i = tour[ii]
@@ -33,13 +24,52 @@ def plotTour(ax, tour):
 		ax.hold(True)
 
 
+statefile = sys.argv[1]
+enefile = sys.argv[2]
+enesurf_filename = sys.argv[3]
+makeMovie = bool(int(sys.argv[4]))
+movie_filename = sys.argv[5]
+
+state = np.loadtxt(statefile)
+ene = np.loadtxt(enefile)
+
+# sampling in distance space
 fig = plt.figure(figsize = (5,5), facecolor = 'w', edgecolor = 'w')
 ax = fig.add_subplot(1,1,1)
+ax.plot(ene, 'k-', linewidth = 3)
+ax.set_xlabel('iteration', fontsize = 20); ax.set_ylabel('tour length', fontsize = 20)
+plt.tight_layout()
+plt.savefig(enesurf_filename)
 
-s = [0, 12, 1, 14, 8, 4, 6, 2, 11, 13, 9, 7, 5, 3, 10, 0]
-plotTour(ax, s)
-print calcEne(s)
+# movie
+if makeMovie:
+	fig = plt.figure(figsize = (10,5), facecolor = 'w', edgecolor = 'w')
+	framestep = 10
+	for i in range(len(state)):
+		if i % framestep: continue
+	
+		timeslice = range(i)
+		stateslice = state[:i]
+		eneslice = ene[:i]
 
+		ax1 = fig.add_subplot(1,2,1)
+		for tour in stateslice:
+			plotTour(ax1, tour)
+			ax1.hold(False)
 
-plt.show()
+		ax2 = fig.add_subplot(1,2,2)
+		ax2.plot(timeslice, eneslice, linestyle = 'none', color = 'red', marker = 'o', markersize = 5)
+		if i == 0:
+			ax2.set_xlabel('iteration', fontsize = 20); ax2.set_ylabel('tour length', fontsize = 20)
+		ax2.hold(True)
+	
+		plt.tight_layout()
+		plt.savefig( 'frame_%d.png' % (int(i/framestep) + 1) )
+
+	cmdstring = 'avconv -i "frame_%d.png" -r 25 -c:v libx264 -crf 10 -pix_fmt yuv420p ' + movie_filename
+	os.system(cmdstring)
+
+	for i in range(len(state)):
+		if i % framestep: continue
+		os.remove( 'frame_%d.png' % (int(i/framestep) + 1) )
 
---
+++ final_project/TSP/rex.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import os, sys, random, copy
+import numpy as np
+
+sys.path.append(os.path.abspath('../')) ; import rexlib
+
+coords = np.loadtxt('p01_xy.txt', skiprows = 1) 
+dist = np.loadtxt('p01_d.txt', skiprows = 1)
+ncities = len(coords)
+
+def Efunc(tour):
+	return sum( [ dist[tour[i], tour[i+1] ] for i in range(ncities) ] )
+
+def tour2str(tour):
+	return ' '.join(str(x) for x in tour) + '\n'
+
+class TSP(rexlib.Replica):
+	def Run(self, Files, RunSteps, StepFreq):
+		self.kB = 1.0
+		
+		RunSteps = int(RunSteps) ; StepFreq = int(StepFreq)
+		InitDataFile, StateFile, EneFile, OutputDataFile = Files
+
+		tour = [int(x) for x in np.loadtxt(InitDataFile)]
+		Ene = Efunc(tour)
+		MAXSWAP = 5
+
+		# write initial co-ordinate
+		file(StateFile, 'a').write(tour2str(tour))
+		file(EneFile, 'a').write('%g\n' % Ene)
+		
+		for n in range(RunSteps):
+			
+			tour_new = copy.copy(tour)
+			isSwapped = []
+			for swap in range(MAXSWAP):
+				ii = np.random.choice(range(ncities-1))
+				jj = np.random.choice([ii-1, ii + 1])
+				if jj > ncities: jj = 0
+				elif jj < 0: jj = ncities-1
+				if isSwapped and ( isSwapped.__contains__((ii, jj)) or isSwapped.__contains__((jj, ii)) ): continue
+				tour_new[ii], tour_new[jj] = tour_new[jj], tour_new[ii]
+			
+			Ene_new = Efunc(tour_new)
+			Delta = (Ene_new - Ene) / (self.kB * self.Temp)
+			pacc = min(1.0, np.exp(-Delta))
+			if pacc > random.random():
+				tour = tour_new
+				Ene = Ene_new
+			
+			if not n % StepFreq:
+				str_tour  = ' '.join(str(x) for x in tour) + '\n'
+				file(StateFile, 'a').write(tour2str(tour))
+				file(EneFile, 'a').write('%g\n' % Ene)
+
+		file(OutputDataFile, 'w').write(tour2str(tour))
+		for thisfile in Files: file(thisfile, 'r').close()
+
+
+### MAIN
+str_tour0 = tour2str(range(ncities) + [0]) ; file('init.dat', 'w').write(str_tour0)
+Temps = [10, 20, 30, 40]
+rex = rexlib.REX(ReplicaClass = TSP, Temps = Temps, EquilSteps = 1e3, ProdSteps = 2e3, StepFreq = 10, SwapSteps = 20, SwapsPerCycle = 3, Verbose = False)
+rex.Run()
+rex.demux(10)
+
